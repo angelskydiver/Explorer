f56905c1e28b2a38b7ccf8e29646ed0569a508ff
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2022-03-14 09:22:16 +0000

    feat: expand Explorer application flow

---
+++ types/summary.ts
@@ -0,0 +1,61 @@
+export interface SummaryType {
+  totalTxCount: number;
+  totalTxVolumeUSD: number;
+  connectedWallets: number;
+  supportedDexes: number;
+  supportedBridges: number;
+  supportedChains: number;
+  lastUpdateDate: string;
+  dailyInterval: DailyIntervalType[];
+}
+
+export interface DailyIntervalType {
+  date: string;
+  count: number;
+  volumeUSD: number;
+}
+
+export enum BreakDownList {
+  'None' = 'TOTAL',
+  'Source chain' = 'SOURCE',
+  'Destination chain' = 'DESTINATION',
+}
+
+export interface DailySummaryType {
+  count: number;
+  date: string;
+  uniqueWallets: number;
+  volume: number;
+  bucket: string;
+}
+
+export interface DailySummaryOption {
+  days: number;
+  breakDownBy: BreakDownList;
+  source?: string;
+  destination?: string;
+}
+
+export interface BlockchainMap {
+  key: string;
+  value: number;
+}
+
+export interface BlockchainPathMap {
+  key: {
+    source: string;
+    destination: string;
+  };
+  value: number;
+}
+
+export interface TopListSummaryType {
+  topDestinationByTxCount: BlockchainMap[];
+  topDestinationByVolume: BlockchainMap[];
+  topSourceByTxCount: BlockchainMap[];
+  topSourceByVolume: BlockchainMap[];
+  topPathsByTxCount: BlockchainPathMap[];
+  topPathsByVolume: BlockchainPathMap[];
+}
+
+export type StatisticDaysFilter = 7 | 30 | 90;
