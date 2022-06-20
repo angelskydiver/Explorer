13858c51091e36f8d7b76e23eb14be32d1ce8020
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2022-06-20 08:24:35 +0000

    fix: polish Explorer implementation

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
+import { ReactNode, useEffect } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+export default function App({ Component, pageProps }: AppProps): ReactNode {
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      document.documentElement.classList.add('fonts-loaded');
+    });
+  }, []);
+  return (
+    <>
+      <NextNProgress color="#00A9BB" height={2} showOnShallow={true} />
+
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
+}
