ebfaba192ebe9d48d97d03f32b3c02079ba5b4f2
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2023-03-15 10:31:39 +0000

    feat: expand Explorer application flow

---
+++ components/common/Layout/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import { PropsWithChildren } from 'react';
+import Navbar from '../Navbar';
+import { LayoutProps } from './Layout.type';
+import Footer from '../Footer';
+
+function Layout(props: PropsWithChildren<LayoutProps>) {
+  return (
+    <div className="max-h-full w-full">
+      <Head>
+        <title>{props.title}</title>
+      </Head>
+      <Navbar theme={'light'} hasSearchInput={props.hasSearchInput} />
+      <main className="w-full min-h-600 md:min-h-[800px] pb-30 md:pb-50 bg-green-background bg-cover bg-center">
+        {props.children}
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
