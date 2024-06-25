799f1f4db0fdba6af26f4cd072c37493ab1d1537
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2024-06-25 10:20:12 +0000

    fix: polish Explorer implementation

---
+++ components/detail/SwapSteps/TransactionURL.tsx
@@ -0,0 +1,63 @@
+import React, { Fragment } from 'react';
+import IconStatus from 'components/common/IconStatus';
+import { TransactionURLProps } from './SwapSteps.type';
+import { SwapStatus } from 'types';
+import ButtonCopyIcon from 'components/common/ButtonCopyIcon';
+import { LinkIcon } from 'components/icons';
+import TransactionURLMobileItem from './TransactionURLMobileItem';
+import { CapitalizeFirstLetter } from 'utils/capitalizeFirstLetter';
+import Tooltip from 'components/common/Tooltip';
+import Link from 'next/link';
+
+function TransactionURL(props: TransactionURLProps) {
+  const { explorerUrls, status } = props;
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <>
+      {explorerUrls.map((exploreItem, index) => {
+        const { url, description } = exploreItem;
+        const overrideDescription = `${
+          description ? CapitalizeFirstLetter(description) : 'Swap'
+        } Transaction`;
+        const transactionStatus: SwapStatus =
+          index === explorerUrls.length - 1 ? status : 'success';
+        return (
+          <Fragment key={url}>
+            <TransactionURLMobileItem
+              description={overrideDescription}
+              transactionStatus={transactionStatus}
+              url={url}
+            />
+            <div
+              key={`transaction-${index}`}
+              className="mb-10 bg-modal-background px-10 py-12 rounded-micro hidden md:flex items-center justify-between">
+              <div className="flex items-center">
+                <IconStatus status={transactionStatus} />
+                <span className="pl-5 text-14 text-primary-500">
+                  {overrideDescription}
+                </span>
+              </div>
+              <div className="flex items-center">
+                <ButtonCopyIcon
+                  className="mr-5"
+                  tooltipText="Copy Transaction"
+                  text={url}
+                />
+                <Tooltip label="View Transaction">
+                  <Link href={url} target="_blank" onClick={handleClick}>
+                    <LinkIcon className="text-neutral-400 hover:text-hoverIcon" />
+                  </Link>
+                </Tooltip>
+              </div>
+            </div>
+          </Fragment>
+        );
+      })}
+    </>
+  );
+}
+
+export default TransactionURL;
